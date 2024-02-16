804074e89eb46778f16348be39ebba2489396bed
Kirill <galzuris@gmail.com>
2024-02-17 04:35:03 +0700

    update

---
+++ README.md
@@ -0,0 +1,5 @@
+# ResCraft J2ME
+
+2D blocky survival game, with craft, mobs and base building
+
+[Project page](https://galzuris.com/p/rescraft_java)
--- src/com/galzuris/rescraft/Game.java
+++ src/com/galzuris/rescraft/Game.java
@@ -2,8 +2,6 @@ package com.galzuris.rescraft;
 
 import java.util.Vector;
 
-import javax.microedition.lcdui.Graphics;
-
 import com.galzuris.utils.Text;
 import com.galzuris.utils.YamlParser;
 import com.galzuris.utils.YamlResult;
@@ -12,10 +10,6 @@ public class Game {
 	private static Game instance;
 	private static YamlResult trans = null;
 	private static String transTag = "en";
-	
-	private float posx = 60;
-	private float posy = 60;
-	private final float speed = 128;
 
 	public Game() {
 		Game.instance = this;
@@ -26,25 +20,11 @@ public class Game {
 	}
 
 	public void init() {
-		Log.write("[game] init");		
+		Log.write("[game] init");
 		this.loadTranslations("/configs/translate.yml");
 	}
 
-	public void update(final float delta) {
-		final GameEngineCanvas c = GameEngineCanvas.getInstance(); 
-		final Graphics g = c.getGraphics();
-		final String str = Game.tr("title");
-		
-		g.setColor(Const.ColorBlack);
-		g.fillRect(0, 0, c.getWidth(), c.getHeight());
-		g.setColor(Const.ColorWhite);
-		g.fillRoundRect((int) posx, (int) posy, Const.BlockSize, Const.BlockSize, Const.BlockSizeHalf, Const.BlockSizeHalf);
-		g.drawString(str, 5, 5, 0);
-		
-		if (c.isActionPressed(Const.KeyActionLeft)) posx -= delta * speed;
-		if (c.isActionPressed(Const.KeyActionRight)) posx += delta * speed;
-		if (c.isActionPressed(Const.KeyActionUp)) posy -= delta * speed;
-		if (c.isActionPressed(Const.KeyActionDown)) posy += delta * speed;
+	public void update(final float delta) {		
 	}
 	
 	public static String tr(String key) {
